94fa8c7036064f66719827c5430c7e3c9dfc0eb7
Miguel <miguelpiresamaro@gmail.com>
2021-03-27 13:00:19 +0000

    Home Screen with some info about the app

---
+++ app/src/main/java/com/example/smartstop/HomeScreen.java
@@ -0,0 +1,78 @@
+package com.example.smartstop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class HomeScreen extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private LinearLayout linearDotsLayout;
+
+    private TextView[] dots;
+
+    private SliderAdapter sliderAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        viewPager = findViewById(R.id.sideViewPaper);
+        linearDotsLayout = findViewById(R.id.dotsLayout);
+
+        sliderAdapter = new SliderAdapter(this);
+
+        viewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator(0);
+
+        viewPager.addOnPageChangeListener(viewListener);
+
+    }
+
+    public void addDotsIndicator(int position) {
+
+        dots = new TextView[3];
+        linearDotsLayout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.colorGray));
+
+            linearDotsLayout.addView(dots[i]);
+
+        }
+
+        if (dots.length > 0) {
+
+            dots[position].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotsIndicator(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
--- app/src/main/java/com/example/smartstop/MainActivity.java
+++ app/src/main/java/com/example/smartstop/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, MapScreen.class);
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
                 startActivity(intent);
                 finish();
             }
